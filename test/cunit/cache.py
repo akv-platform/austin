c4c33541f9172d483399dda15d948f0f1bec5294
Sergey Dolin <sergey.dolin@gmail.com>
2022-07-21 13:21:26 +0200

    run

--- test/cunit/cache.py
+++ test/cunit/cache.py
@@ -7,7 +7,7 @@ CFLAGS = ["-g", "-fprofile-arcs", "-ftest-coverage"]
 print("start compile " + __name__)
 print(__name__)
 print(SRC / Path(__file__).stem)
-sys.modules[__name__] = SRC / Path(__file__).stem
-CModule.compile(SRC / Path(__file__).stem, cflags=CFLAGS)
+#sys.modules[__name__] = SRC / Path(__file__).stem
+sys.modules[__name__] = CModule.compile(SRC / Path(__file__).stem, cflags=CFLAGS)
 print("end compile")
 print(sys.modules)
