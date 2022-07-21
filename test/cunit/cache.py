22e3fd9f7fa7fcab626b01f55fd2f6f82b074b1d
Sergey Dolin <sergey.dolin@gmail.com>
2022-07-21 09:25:52 +0200

    run without compile

--- test/cunit/cache.py
+++ test/cunit/cache.py
@@ -8,5 +8,6 @@ print("start compile " + __name__)
 print(__name__)
 print(SRC / Path(__file__).stem)
 sys.modules[__name__] = SRC / Path(__file__).stem
+print(sys.modules)
 #CModule.compile(SRC / Path(__file__).stem, cflags=CFLAGS)
 print("end compile")
