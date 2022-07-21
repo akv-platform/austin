992c0fb1fe9edc80c6acece56343388176bfb045
Sergey Dolin <sergey.dolin@gmail.com>
2022-07-21 09:19:39 +0200

    run without compile

--- test/cunit/cache.py
+++ test/cunit/cache.py
@@ -4,8 +4,9 @@ from test.cunit import SRC, CModule
 
 CFLAGS = ["-g", "-fprofile-arcs", "-ftest-coverage"]
 
-print("start compile")
+print("start compile " + __name__)
 print(__name__)
 print(SRC / Path(__file__).stem)
-sys.modules[__name__] = CModule.compile(SRC / Path(__file__).stem, cflags=CFLAGS)
+sys.modules[__name__] = SRC / Path(__file__).stem
+#CModule.compile(SRC / Path(__file__).stem, cflags=CFLAGS)
 print("end compile")
