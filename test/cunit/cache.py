3262fc5067bfec7b884f2db7306e34e44f5f9542
Sergey Dolin <sergey.dolin@gmail.com>
2022-07-21 08:01:03 +0200

    repro compile

--- test/cunit/cache.py
+++ test/cunit/cache.py
@@ -4,4 +4,8 @@ from test.cunit import SRC, CModule
 
 CFLAGS = ["-g", "-fprofile-arcs", "-ftest-coverage"]
 
+print("start compile")
+print(__name__)
+print(SRC / Path(__file__).stem)
 sys.modules[__name__] = CModule.compile(SRC / Path(__file__).stem, cflags=CFLAGS)
+print("start compile")
