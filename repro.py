eb9382a94497d4392cf0aaeab80efead8a13d192
Sergey Dolin <sergey.dolin@gmail.com>
2022-08-03 23:49:53 +0200

    print repro

--- repro.py
+++ repro.py
@@ -4,7 +4,7 @@ from test.cunit.cache import QueueItem
 value = C.malloc(16)
 queue_item = QueueItem(value, 42)
 print("----------> ")
-print(queue_item)
+#print(queue_item)
 #print(queue_item.key)
 #print(queue_item.value)
 print("^^^^^^^^^^^^")
