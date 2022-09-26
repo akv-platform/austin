988a074bae2f6c8db51f8c1e29d3ded37d7ad70f
Sergey Dolin <sergey.dolin@gmail.com>
2022-09-26 08:50:55 +0200

    free

--- repro.py
+++ repro.py
@@ -8,7 +8,7 @@ print(dir(test.cunit.cache.QueueItem))
 
 
 print("free __annotations__", flush=True)
-print(C.free.__annotations__, flush=True)
+#print(C.free.__annotations__, flush=True)
 
 print("QueueItem.new __annotations__", flush=True)
 print(test.cunit.cache.QueueItem.new.__annotations__)
